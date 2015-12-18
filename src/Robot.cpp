64314dae603ba1fcf03a7e464e8f17f518c94b2d
Tony Brar <tonycbrar@gmail.com>
2015-12-17 20:06:28 -0700

    Added robot skeleton to robot.cpp

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,8 +1,49 @@
 #include "Robot.h"
-
+#include "stdio.h"
 #include <HAL/HAL.hpp>
 
 Robot::Robot() {
+}
+
+// robot init implementation
+void Robot::RobotInit() {
+	std::cout << "Entered RobotInit()" << std::endl;
+}
+
+// disabled init implementation
+void Robot::DisabledInit() {
+	std::cout << "Entered DisabledInit()" << std::endl;
+}
+
+// autonomous init implementation
+void Robot::AutonomousInit() {
+	std::cout << "Entered AutonomousInit()" << std::endl;
+}
+
+// teleop init implementation
+void Robot::TeleopInit() {
+	std::cout << "Entered TeleopInit()" << std::endl;
+}
+
+// test init implementation
+void Robot::TestInit() {
+	std::cout << "Entered TestInit()" << std::endl;
+}
+
+// disabled periodic implementation
+void Robot::DisabledPeriodic() {
+}
+
+// autonomous periodic implementation
+void Robot::AutonomousPeriodic() {
+}
+
+// teleop periodic implementation
+void Robot::TeleopPeriodic() {
+}
+
+// test periodic implementation
+void Robot::TestPeriodic() {
 }
 
 START_ROBOT_CLASS(Robot)
--- src/Robot.h
+++ src/Robot.h
@@ -6,6 +6,16 @@
 class Robot: public IterativeRobot {
 public:
 	Robot();
+	virtual void RobotInit();
+	virtual void DisabledInit();
+	virtual void AutonomousInit();
+	virtual void TeleopInit();
+	virtual void TestInit();
+	virtual void DisabledPeriodic();
+	virtual void AutonomousPeriodic();
+	virtual void TeleopPeriodic();
+	virtual void TestPeriodic();
 };
+
 
 #endif /* SRC_ROBOT_H_ */
