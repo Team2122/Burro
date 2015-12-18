e51d9bf125726d047e9b61f92898ff9f7c4a8400
Tony Brar <tonycbrar@gmail.com>
2015-12-17 19:35:25 -0700

    Added Robot class

---
+++ src/Robot.cpp
@@ -0,0 +1,8 @@
+#include "Robot.h"
+
+#include <HAL/HAL.hpp>
+
+Robot::Robot() {
+}
+
+START_ROBOT_CLASS(Robot)
---
+++ src/Robot.h
@@ -0,0 +1,11 @@
+#ifndef SRC_ROBOT_H_
+#define SRC_ROBOT_H_
+
+#include <IterativeRobot.h>
+
+class Robot: public IterativeRobot {
+public:
+	Robot();
+};
+
+#endif /* SRC_ROBOT_H_ */
