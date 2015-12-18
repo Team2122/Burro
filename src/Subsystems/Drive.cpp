49dbabbca0f72771cff3ab3498547c9a83206226
John Chen <jjchen2019@gmail.com>
2015-12-17 20:53:34 -0700

    Added the drive subsystem

---
+++ src/Subsystems/Drive.cpp
@@ -0,0 +1,35 @@
+#include "Drive.h"
+#include <Victor.h>
+
+Drive::Drive() :
+		Subsystem("Drive") {
+	left1 = new Victor(0);
+	left2 = new Victor(1);
+	left3 = new Victor(2);
+	right1 = new Victor(3);
+	right2 = new Victor(4);
+	right3 = new Victor(5);
+
+}
+
+Drive::~Drive() {
+	delete left1;
+	delete left2;
+	delete left3;
+	delete right1;
+	delete right2;
+	delete right3;
+}
+
+void Drive::SetSpeeds(float leftSpeed, float rightSpeed) {
+	left1->Set(-leftSpeed);
+	left2->Set(-leftSpeed);
+	left3->Set(-leftSpeed);
+	right1->Set(rightSpeed);
+	right2->Set(rightSpeed);
+	right3->Set(rightSpeed);
+}
+
+void Drive::ResetSpeeds() {
+	SetSpeeds(0, 0);
+}
---
+++ src/Subsystems/Drive.h
@@ -0,0 +1,23 @@
+#ifndef SRC_SUBSYSTEMS_DRIVE_H_
+#define SRC_SUBSYSTEMS_DRIVE_H_
+
+#include <Commands/Subsystem.h>
+
+class Victor;
+
+class Drive: public Subsystem {
+public:
+	Drive();
+	~Drive();
+	void SetSpeeds(float leftSpeed, float rightSpeed);
+	void ResetSpeeds();
+private:
+	Victor* left1;
+	Victor* left2;
+	Victor* left3;
+	Victor* right1;
+	Victor* right2;
+	Victor* right3;
+};
+
+#endif /* SRC_SUBSYSTEMS_DRIVE_H_ */
