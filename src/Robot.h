1ee542e0cd2bd2d8c982817c7b91e4e341cda835
amikhalev <alexmikhalevalex@gmail.com>
2015-12-21 19:01:37 -0700

    Added robot skeleton to robot.cpp

--- src/Robot.h
+++ src/Robot.h
@@ -6,6 +6,18 @@
 class Robot: public IterativeRobot {
 public:
 	Robot();
+
+	void RobotInit() override;
+
+	void DisabledInit() override;
+	void AutonomousInit() override;
+	void TeleopInit() override;
+	void TestInit() override;
+
+	void DisabledPeriodic() override;
+	void AutonomousPeriodic() override;
+	void TeleopPeriodic() override;
+	void TestPeriodic() override;
 };
 
 #endif /* SRC_ROBOT_H_ */
