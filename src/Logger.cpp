a2ce62ededd189cb96e6f91ef19d11bc45c183d7
Tony Brar <tonycbrar@gmail.com>
2015-12-28 15:57:51 -0700

    Fixed timestamp formatting

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,27 +2,32 @@
 
 #include "Timer.h"
 #include <iostream>
+#include <iomanip>
 
 Logger::Logger(const std::string& loggerSource) {
 	this->source = loggerSource;
 }
 
 void Logger::Info(const std::string& message) {
-	std::cout << Timer::GetFPGATimestamp() << " [INFO] " << source << " - "
+	std::cout << std::fixed << std::setprecision(3) << std::setw(8)
+			<< Timer::GetFPGATimestamp() << " [INFO] " << source << " - "
 			<< message << std::endl;
 }
 
 void Logger::Warn(const std::string& message) {
-	std::cout << Timer::GetFPGATimestamp() << " [WARN] " << source << " - "
+	std::cout << std::fixed << std::setprecision(3) << std::setw(8)
+			<< Timer::GetFPGATimestamp() << " [WARN] " << source << " - "
 			<< message << std::endl;
 }
 
 void Logger::Error(const std::string& message) {
-	std::cout << Timer::GetFPGATimestamp() << " [ERROR] " << source << " - "
+	std::cout << std::fixed << std::setprecision(3) << std::setw(8)
+			<< Timer::GetFPGATimestamp() << " [ERROR] " << source << " - "
 			<< message << std::endl;
 }
 
 void Logger::State(const std::string& message) {
-	std::cout << Timer::GetFPGATimestamp() << " [STATE] " << source << " - "
+	std::cout << std::fixed << std::setprecision(3) << std::setw(8)
+			<< Timer::GetFPGATimestamp() << " [STATE] " << source << " - "
 			<< message << std::endl;
 }
